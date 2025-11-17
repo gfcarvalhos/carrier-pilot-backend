a0fa6c3907b0a4b65a39d10071b1d69f3c16eb3b
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-11-17 09:06:40 -0300

    feat: criacao do app para carreira e modelos

---
+++ carreiras/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from usuarios.models import Usuario
+
+class Atividade(models.Model):
+  titulo = models.CharField(max_length=100)
+  descricao = models.CharField(max_length=300)
+  categoria = models.CharField(max_length=100)
+  duracao_minutos = models.IntegerField()
+  prioridade = models.IntegerField(choices= [
+    (1, "Alta"),
+    (2, "Média"),
+    (3, "Baixa"),
+  ], default=3)
+
+  def __str__(self):
+    return f"{self.titulo} - {self.categoria}"
+
+
+class Recomendacao(models.Model):
+  usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+  atividade = models.ForeignKey(Atividade, on_delete=models.SET_NULL, null=True)
+
+  tema = models.CharField(max_length=200)
+  subtema = models.CharField(max_length=200, blank=True, null=True)
+
+  descricao = models.TextField()
+  recursos = models.JSONField(blank=True, null=True) #Lista de links/recursos
+  status = models.CharField(
+    max_length=20,
+    choices=[
+      ('pendente', 'Pendente'),
+      ('aceita', 'Aceita'),
+      ('ignorada', 'Ignorada')
+    ],
+    default = 'pendente'
+  ) 
+
+  #Dados IA
+  payload_ia = models.JSONField(blank=True, null=True)
+  explicacao_ia = models.TextField(blank=True, null=True)
+  score_relevancia = models.FloatField(default=0)
+
+  data_gerada = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.tema} - {self.subtema or ''} ({self.usuario})"
+
+
+class Progresso(models.Model):
+  usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+  atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE, null=True)
+  data_concluida = models.DateTimeField(blank=True, null=True)
+  status = models.CharField(
+    max_length=20,
+    choices=[
+      ('pendente', 'Pendente'),
+      ('em_andamento', 'Em andamento'),
+      ('concluida', 'Concluída')
+    ],
+    default = 'pendente'
+  )
+
+  def __str__(self):
+    return f"{self.usuario} - {self.atividade} : {self.status}"
--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'usuarios',
-    'django_filters'
+    'carreiras',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
