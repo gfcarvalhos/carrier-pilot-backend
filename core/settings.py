86ad69515d0dd86d8c9256a8b43c326723405904
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-11-30 20:47:34 -0300

    fix: permission

--- core/settings.py
+++ core/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
